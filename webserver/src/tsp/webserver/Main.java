d97e7f1a31ed8eb0bbb0c9deafdeda243319b714
Dimitri78000 <dimi_du_24@hotmail.fr>
2018-02-07 15:26:05 +0100

    3

--- webserver/src/tsp/webserver/Main.java
+++ webserver/src/tsp/webserver/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		Film Black_Sheep = new Film("Black Sheep", 2005);
-		Film Black_Sheep_2 = new Film("Black Sheep", 2006);
-		Black_Sheep.equals(Black_Sheep_2);
+		Film Black_Sheep_2 = new Film("Black Sheep", 2005);
+		System.out.println(Black_Sheep.equals(Black_Sheep_2));
 		
 	}
 
