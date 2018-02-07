e220f60a0bb122580ed1c55edb49764bc1a47e26
Dimitri78000 <dimi_du_24@hotmail.fr>
2018-02-07 15:22:23 +0100

    2

---
+++ webserver/src/tsp/webserver/Film.java
@@ -0,0 +1,29 @@
+package tsp.webserver;
+import java.util.HashMap;
+
+public class Film 
+{
+	String title;
+	int year;
+	Film(String my_title,int my_year)
+	{
+		this.title=my_title;
+		this.year=my_year;
+	}
+	public String toString()
+	{
+		return this.title + "(" + this.year +") ";
+	}
+	public boolean equals(Object o)
+	{
+		if (o instanceof Film)
+		{
+			Film my_film =(Film)o;
+			return ((this.title == my_film.title ) && (this.year==my_film.year) );
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
---
+++ webserver/src/tsp/webserver/Main.java
@@ -0,0 +1,13 @@
+package tsp.webserver;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		Film Black_Sheep = new Film("Black Sheep", 2005);
+		Film Black_Sheep_2 = new Film("Black Sheep", 2006);
+		Black_Sheep.equals(Black_Sheep_2);
+		
+	}
+
+}
