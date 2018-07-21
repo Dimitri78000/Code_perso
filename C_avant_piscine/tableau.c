f6ad0e851d33ebde52e1be57bf576787e34e31e8
Dimitri78000 <dimitrileurs@MacBook-Air-de-Irina.local>
2018-07-21 23:36:15 +0200

    Ajout projet Code Block

--- C_avant_piscine/tableau.c
+++ C_avant_piscine/tableau.c
@@ -7,7 +7,8 @@ int main(int argc, char *argv[])
 {
 	printf("\n Debut fonction \n");
 	// Declaration des variables
-
+	int tableau[5]={1,2,3,4,5};
+	afficherTableau(tableau, 5);
 
 
 	printf("\n Fin fonction \n"); 
